ca1248c698b69d3872777c6d40c9a6e080c2c944
Tix-learn-dev <t.rafanoharantsoa@codeur.online>
2020-05-25 10:17:02 +0200

    first commit

---
+++ myfirstinteg/footer.php
@@ -0,0 +1,60 @@
+
+<footer >
+
+        <section id="menu4">
+                    <div class="container  bg-secondary pt-2">
+                      <div class="page-header text-center text-light ">
+                        <h1>MENU 4</h1>
+                        <p>Ambitioni dedisse scripsisse iudicaretur.</p>
+                        <hr class="rupture">
+                      </div>
+
+                      <form >
+                        <div class="form-group">
+                              <label for="message"> Nom  </label>
+                              <input type="text" class="form-control" name="nom" placeholder="Nom" >
+                        </div>
+                        <div class="form-group">
+                              <label for="message"> Sujet </label>
+                              <input type="text" class="form-control" name="nom" placeholder="Sujet" >
+                        </div>
+                        <div class="form-group">
+                              <label for="message"> Adresse email </label>
+                              <input type="email" class="form-control" name="nom" placeholder="name@example.com" > <br>
+                        </div>
+                        <div class="form-group">
+                                <label for="exampleFormControlTextarea1">Ecrivez votre message en dessous</label>
+                                <textarea class="form-control" id="message" rows="3"></textarea>
+                        </div>
+
+                              <input type="submit" name="submit"value="envoyer">
+                      </form>
+                      <h2 class="text_center text-light mt-4"> Mes réseaux sociaux </h2>
+                      <div class="row mt-2 justify-content-center">
+
+                        <div class="col 2  col-md-1">
+                          <a class="linkedin" href="#"> <img src="../images/linkedin.jpg" class="img-fluid d-block mx-auto social" alt="logolinkedin"> </a>
+                        </div>
+                        <div class="col 2 col-md-1">
+                          <a class="pinterest" href="#"> <img src="../images/pinterest.jpg" class="img-fluid d-block mx-auto social" alt="logopinterest"> </a>
+                        </div>
+                        <div class="col 2 col-md-1">
+                          <a class="facebook" href="#"> <img src="../images/facebook.jpg" class="img-fluid d-block mx-auto social" alt="logofacebook"> </a>
+                        </div>
+                        <div class="col 2 col-md-1">
+                          <a class="twitter" href="#"> <img src="../images/twitter.jpg" class="img-fluid d-block mx-auto social" alt="disk"> </a>
+                        </div>
+                        <div class="col 2 col-md-1">
+                          <a class="gplus" href="#"> <img src="../images/gplus.jpg" class="img-fluid d-block mx-auto social" alt="disk"> </a>
+                        </div>
+                      </div>
+                    </div>
+            </section>
+</footer >
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ myfirstinteg/header.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable =no">
+    <title>Myfirstinteg</title>
+    <link rel="stylesheet" href="main1.css">
+    <link href="roboto/stylesheet.css" rel="stylesheet" type="text/css" >
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+  </head>
+
+  <body class="bg-white">
+
+      <header>
+
+                          <nav class="container navbar navbar-expand-md navbar-dark bg-dark ">
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                              <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarNav">
+                              <ul class="navbar-nav  text-uppercase w-100 justify-content-around align-items-center ">
+                                <li class="nav-item ">
+                                  <a class="nav-link"  href="#accueil"> Accueil </a>
+                                </li>
+                                <li class="nav-item ">
+                                  <a class="nav-link" href="#menu2"> Portfolio </a>
+                                </li>
+                                <li class="nav-item ">
+                                  <a class="nav-link" href="#menu1"><img src="../logo/thlogopaint.png" class="img-fluid" title="Tsiry RAFANOHARANTSOA" h-75/></a>
+                                </li>
+                                <li class="nav-item ">
+                                  <a class="nav-link" href="#menu4">Contact</a>
+                                </li>
+                                <li class="nav-item ">
+                                  <a class="nav-link" href="#menu3">Curriculum vitae</a>
+                                </li>
+                              </ul>
+                            </div>
+                          </nav>
+
+      </header>
+
+      <section id="accueil">
+                <div class="container bg-light shadow shadow-md pt-2 pb-4 ">
+                  <div class="page-header text-center ">
+                    <h1>MON PROJET</h1>
+                    <p>Ambitioni dedisse scripsisse iudicaretur.</p>
+                    <hr class="rupture">
+                  </div>
+                    <div id="carousel" class="carousel slide" data-interval="3000" data-ride="carousel" >
+                        <ol class="carousel-indicators">
+                          <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                          <li data-target="#carousel" data-slide-to="1"></li>
+                          <li data-target="#carousel" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                          <div class="carousel-item active ">
+                            <img src="../images/jeffersonsantos.jpg" class="d-block img-fluid"  alt="jeffersonsantos">
+                              <div class="carousel-caption d-none d-md-block">
+                                <h5>Photo 1</h5>
+                                <p>Description</p>
+                              </div>
+                          </div>
+                          <div class="carousel-item">
+                            <img src="../images/mariusniveri.jpg" class="d-block img-fluid" alt="mariusniveri">
+                              <div class="carousel-caption d-none d-md-block">
+                                <h5>Photo 2</h5>
+                                <p>Description</p>
+                              </div>
+                          </div>
+                          <div class="carousel-item">
+                            <img src="../images/fondwebdesign.jpg" class="d-block img-fluid" alt="fondwebdesign">
+                              <div class="carousel-caption d-none d-md-block">
+                                <h5>Photo 3</h5>
+                                <p>Description</p>
+                              </div>
+                          </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="sr-only">Next</span>
+                        </a>
+                      </div>
+                        <p class="text-center text-uppercase mt-4 mb-4">Lorem ipsum dolor sit amet</p>
+                    </div>
+                </div>
+
+      </section>
+
+      <hr class="rupture">
+
+      <section id="menu1" >
+                    <div class="container bg-light pt-2 shadow shadow-md  sectioncontainer ">
+                      <div class="page-header text-center  ">
+                        <h1>MENU 1</h1>
+                        <p >Ambitioni dedisse scripsisse iudicaretur.</p>
+                        <hr class="rupture">
+                      </div>
+                      <div class="row justify-content-around ">
+                        <div class=" col-sm-12 col-md-6 col-lg-2 col-xl-2  ">
+                          <a class="foodicone" href="#"> <img src="../images/pasta.jpg" class="img-fluid d-block mx-auto" alt="pasta"> </a>
+                          <h2 class="texte-center"> TITRE1 </h2>
+                          <h3 class="texte-center"> SOUSTITRE1 </h3>
+                          <hr class="rupture">
+                          <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div class=" col-sm-12 col-md-6 col-lg-2 col-xl-2">
+                          <a class="tv" href="#"> <img src="../images/tv.jpg" class="img-fluid d-block mx-auto " alt="télé"> </a>
+                          <h2 class="texte-center"> TITRE1 </h2>
+                          <h3 class="texte-center"> SOUSTITRE1 </h3>
+                          <hr class="rupture">
+                          <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div class=" col-sm-12 col-md-6 col-lg-2 col-xl-2">
+                          <a class="clothes" href="#"> <img src="../images/clothes.jpg" class="img-fluid d-block mx-auto" alt="clothes"> </a>
+                          <h2 class="texte-center"> TITRE1 </h2>
+                          <h3 class="texte-center"> SOUSTITRE1 </h3>
+                          <hr class="rupture">
+                          <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div class=" col-sm-12 col-md-6 col-lg-2 col-xl-2">
+                          <a class="disk" href="#"> <img src="../images/disk.jpg" class="img-fluid d-block mx-auto" alt="disk"> </a>
+                          <h2 class="texte-center"> TITRE1 </h2>
+                          <h3 class="texte-center"> SOUSTITRE1 </h3>
+                          <hr class="rupture">
+                          <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                      </div>
+                  </div>
+
+      </section >
+      <hr class="rupture">
+
+      <section id="menu2">
+                    <div class="container bg-light shadow shadow-md  pt-2">
+                        <div class="page-header text-center ">
+                          <h1>MENU 2</h1>
+                          <p>Ambitioni dedisse scripsisse iudicaretur.</p>
+                          <hr class="rupture">
+                        </div>
+
+                          <div class="row justify-content-around mt-5 ">
+                            <div class="col-md-5 col-lg-2 pt-1 mt-5 bg-white  shadow-sm  ">
+                              <div class="thumbnail ">
+                                <a href="../images/zach.jpg">
+                                  <img src="../images/zach.jpg" alt="route" class="img-fluid" style="width:100%">
+                                  <div class="caption">
+                                    <p>Lorem ipsum...</p>
+                                  </div>
+                                </a>
+                              <!--<div class="iconequibougeaupointeur"> </div< -->
+                              </div>
+                            </div>
+                            <div class="col-md-5  col-lg-2 pt-1 mt-5 bg-white  shadow-sm">
+                              <div class="thumbnail">
+                                <a href="../images/zach.jpg">
+                                  <img src="../images/zach.jpg" alt="route" class="img-fluid " style="width:100%">
+                                  <div class="caption">
+                                    <p>Lorem ipsum...</p>
+                                  </div>
+                                </a>
+                              <!--<div class="iconequibougeaupointeur"> </div< -->
+                              </div>
+                            </div>
+                            <div class="col-md-5  col-lg-2  pt-1 mt-5 bg-white  shadow-sm">
+                              <div class="thumbnail">
+                                <a href="../images/jakob.jpg">
+                                  <img src="../images/jakob.jpg" alt="route" class="img-fluid" style="width:100%">
+                                  <div class="caption">
+                                    <p>Lorem ipsum...</p>
+                                  </div>
+                                </a>
+                              <!--<div class="iconequibougeaupointeur"> </div< -->
+                              </div>
+                            </div>
+                            <div class="col-md-5 col-lg-2 pt-1 mt-5 bg-white  shadow-sm">
+                              <div class="thumbnail">
+                                <a href="../images/jakob.jpg">
+                                  <img src="../images/jakob.jpg" alt="route" class="img-fluid" style="width:100%">
+                                  <div class="caption">
+                                    <p>Lorem ipsum...</p>
+                                  </div>
+                                </a>
+                              <!--<div class="iconequibougeaupointeur"> </div< -->
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="row justify-content-around mb-5  ">
+                            <div class="col-md-5  col-lg-2  pt-1 mt-5  bg-white  shadow-sm">
+                              <div class="thumbnail  ">
+                                <a href="../images/zach.jpg">
+                                  <img src="../images/zach.jpg" alt="route" class="img-fluid" style="width:100%">
+                                  <div class="caption">
+                                    <p>Lorem ipsum...</p>
+                                  </div>
+                                </a>
+                              <!--<div class="iconequibougeaupointeur"> </div< -->
+                              </div>
+                            </div>
+                            <div class="col-md-5  col-lg-2  pt-1  mt-5 bg-white  shadow-sm">
+                              <div class="thumbnail">
+                                <a href="../images/zach.jpg">
+                                  <img src="../images/zach.jpg" alt="route" class="img-fluid" style="width:100%">
+                                  <div class="caption">
+                                    <p>Lorem ipsum...</p>
+                                  </div>
+                                </a>
+                              <!--<div class="iconequibougeaupointeur"> </div< -->
+                              </div>
+                            </div>
+                            <div class="col-md-5  col-lg-2  pt-1  mt-5 bg-white  shadow-sm">
+                              <div class="thumbnail">
+                                <a href="../images/jakob.jpg">
+                                  <img src="../images/jakob.jpg" alt="route" class="img-fluid" style="width:100%">
+                                  <div class="caption">
+                                    <p>Lorem ipsum...</p>
+                                  </div>
+                                </a>
+                              <!--<div class="iconequibougeaupointeur"> </div< -->
+                              </div>
+                            </div>
+                            <div class="col-md-5  col-lg-2  pt-1 mt-5 bg-white  shadow-sm">
+                              <div class="thumbnail">
+                                <a href="../images/jakob.jpg">
+                                  <img src="../images/jakob.jpg" alt="route" class="img-fluid" style="width:100%">
+                                  <div class="caption">
+                                    <p>Lorem ipsum...</p>
+                                  </div>
+                                </a>
+                              <!--<div class="iconequibougeaupointeur"> </div< -->
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="textpresentationcontainer">
+                            <div class="textpresentationcontainer2">
+                              <a class="nav-link text-center" href="#"> LIEN POUR TOUT VOIR </a>
+                            </div>
+                          </div>
+                    </div>
+      </section>
+
+      <hr class="rupture">
+
+      <section id="menu3" >
+                    <div class="container bg-light shadow shadow-md pt-2">
+                          <div class="page-header text-center  ">
+                            <h1>MENU 3</h1>
+                            <p>Ambitioni dedisse scripsisse iudicaretur.</p>
+                            <hr class="rupture">
+                          </div>
+
+                          <div class="row justify-content-center align-items-center">
+                              <div class="col-md-5  imgmenu3">
+                                  <a class="imagezach" href="#"><img src="../images/jakob.jpg" class="img-fluid  " alt="route"/></a>
+                              </div>
+                              <div class="col-md-5  ">
+                                      <h1 class="text-left"> TITRE1 </h1>
+                                      <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim i  est laborum.</p>
+                              </div>
+                          </div>
+                          <div class="row justify-content-center align-items-center mt-5 ">
+                              <div class="col-md-5 imgmenu3">
+                                  <a class="imagezach" href="#"><img src="../images/jakob.jpg" class="img-fluid " alt="route"/></a>
+                              </div>
+                              <div class="col-md-5 ">
+                                      <h1 class="text-left"> TITRE1 </h1>
+                                      <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim i  est laborum.</p>
+                              </div>
+                          </div>
+                          <div class="row justify-content-center align-items-center mt-5 ">
+                              <div class="col-md-5 imgmenu3 ">
+                                  <a class="imagezach" href="#"><img src="../images/jakob.jpg" class="img-fluid  " alt="route"/></a>
+                              </div>
+                              <div class="col-md-5">
+                                      <h1 class="text-left"> TITRE1 </h1>
+                                      <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim i  est laborum.</p>
+                              </div>
+                          </div>
+
+                              <div class="col">
+                                <div class="textpresentationcontainer2">
+                                  <a class="nav-link text-center mt-5" href="#"> LIEN POUR TOUT VOIR </a>
+                                </div>
+                              </div>
+                          </div>
+
+      </section >
+
+      <hr class="rupture">
